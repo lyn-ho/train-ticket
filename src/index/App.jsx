2b18c2c2f9fb2542a07044e52d118631ce2d6bab
Lyn Ho <lyn4develop@gmail.com>
2019-12-20 16:29:12 +0800

    feat: 提交按钮控件

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -113,7 +113,7 @@ function App(props) {
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack} />
       </div>
-      <form className="form">
+      <form action="./query.html" className="form">
         {/* <Journey from={from} to={to} exchangeFromTo={doExchangeFromTo} showCitySelector={doShowCitySelector} /> */}
         <Journey from={from} to={to} {...cbs} />
         <DepartData time={departDate} {...departDateCbs} />
