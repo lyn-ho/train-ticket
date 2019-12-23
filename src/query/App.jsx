3c7eecb6fdc04dd2432d0125c442dc2f5e8ca985
Lyn Ho <lyn4develop@gmail.com>
2019-12-23 13:43:07 +0800

    feat: 搜索页 数据结构与模块设计

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,16 +1,26 @@
-// import { connect } from 'react-redux'
+import React from 'react'
+import { connect } from 'react-redux'
 
-// import './App.css'
+import './App.css'
+import Nav from '../common/Nav'
+import List from './List'
+import Bottom from './Bottom'
 
-// function App(props) { }
-
-// export default connect(
-//   function mapStateToProps(state) { },
-//   function mapDispatchToProps(dispatch) { },
-// )(App)
-
-function App() {
-  return null
+function App(props) {
+  return (
+    <div>
+      <Nav />
+      <List />
+      <Bottom />
+    </div>
+  )
 }
 
-export default App
+export default connect(
+  function mapStateToProps(state) {
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch }
+  },
+)(App)
